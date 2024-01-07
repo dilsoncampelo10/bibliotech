d79caf53a99c41a725f6a0ba8d8399588e0793b3
dilsoncampelo10 <dilsono316@gmail.com>
2024-01-07 14:16:14 -0300

    add jetstream

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,13 @@ import path from 'path';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/css/index.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
     resolve: {
         alias: {
-            '@bootstrap': path.resolve(__dirname,'node_modules/bootstrap/dist')
+            '@bootstrap': path.resolve(__dirname, 'node_modules/bootstrap/dist')
         },
     },
 });
